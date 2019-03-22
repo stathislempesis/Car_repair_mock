53d266349205eec80b76153f1098e9452388fa4b
Efstathios Lempesis <lempeg7@gmail.com>
2019-03-22 17:39:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 # import flask dependencies
 from flask import Flask, make_response, jsonify, request
@@ -21,16 +22,17 @@ def results():
     action = req.get('queryResult').get('action')
     
     parameters = req.get('queryResult').get('parameters')
-    print(parameters['car-type'])
-    #if(action=="hours"):
-       # response = make_response(jsonify({'fulfillmentText': 'Our opening hours are Monday - Friday from 9.00 am to 5.00 pm.'}))
-    #elif(action=="repair"):
-        #response = make_response(jsonify({'fulfillmentText': 'Repair.'}))
-    #else:
-        #response = make_response(jsonify({'fulfillmentText': 'Something else.'}))
+    
+    not_available_dates = [datetime.date(2019, 3,26), datetime.date(2019, 3, 27)]
+    
+    if parameters['car-type'] and parameters['service-option'] and parameters['date']:
+       selected_date = datetime.strptime(parameters['date'], '%Y-%m-%dT%H:%M:%S+%M:%S')
+       response = make_response(jsonify({'fulfillmentText': 'Our opening hours are Monday - Friday from 9.00 am to 5.00 pm.'}))
+    else:
+       response = make_response(jsonify({'fulfillmentText': 'Something else.'}))
         
     # return a fulfillment response
-    return make_response(jsonify({'fulfillmentText': 'Yolo.'}))
+    return response
 
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
